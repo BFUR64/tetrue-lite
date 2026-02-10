13d168fe00db7c5a98f7612d23aad168de7ed6ef
Terrance1120 <iterranceevan@gmail.com>
2026-02-10 14:47:03 +0800

    Added shadowJar plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
     id("eclipse")
+    id("com.gradleup.shadow") version "9.3.1"
 }
 
 repositories {
