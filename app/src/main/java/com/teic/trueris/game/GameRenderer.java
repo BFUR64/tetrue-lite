6a542b9744475ebf826f6f4b0449f177c3061c6b
BFUR64 <iterranceevan@gmail.com>
2026-06-01 21:37:51 +0800

    refactor: `GameRenderer` overhauled
    Buffered rendering was removed in favor of the lower-level `BufferedTerminal` implementation in the `tetrue-terminal` library

--- app/src/main/java/com/teic/trueris/game/GameRenderer.java
+++ app/src/main/java/com/teic/trueris/game/GameRenderer.java
@@ -10,12 +10,9 @@ import io.github.bfur64.terminal.interfaces.TerminalBackend;
 import java.util.List;
 
 public class GameRenderer {
-    private static final char EMPTY = ' ';
-    private static final char SOLID = '█';
-    private static final char GHOST = '░';
+    private static final String SOLID = "█";
+    private static final String GHOST = "░";
 
-    private final int BUFFER_HEIGHT;
-    private final int BUFFER_WIDTH;
     private final int BORDER_THICKNESS = 1;
     private final int BORDER_OFFSET = 2;
 
@@ -23,24 +20,15 @@ public class GameRenderer {
     private final GridData gridData;
     private final GameState gameState;
 
-    private RenderCell[][] previousBuffer;
-    private RenderCell[][] currentBuffer;
-
     public GameRenderer(TerminalBackend terminal, GridData gridData, GameState gameState) {
         this.terminal = terminal;
         this.gridData = gridData;
         this.gameState = gameState;
-
-        BUFFER_HEIGHT = terminal.getYSize();
-        BUFFER_WIDTH = terminal.getXSize();
-
-        previousBuffer = new RenderCell[BUFFER_HEIGHT][BUFFER_WIDTH];
-        currentBuffer = new RenderCell[BUFFER_HEIGHT][BUFFER_WIDTH];
-        clearBuffer(previousBuffer);
-        clearBuffer(currentBuffer);
     }
 
     public void update() {
+        terminal.clearScreen();
+
         int gameBorderWidth = Config.gridWidth.get() + BORDER_OFFSET;
         int gameBorderHeight = Config.gridHeight.get() + BORDER_OFFSET;
 
@@ -50,18 +38,18 @@ public class GameRenderer {
 
         // Score & Difficulty / Gravity
         int leftPadding = gameBorderWidth + 1;
-        writeString(leftPadding, 1, "Score: " + gameState.getScore());
+        putString(leftPadding, 1, "Score: " + gameState.getScore());
         if (!Config.showGravity.get()) {
-            writeString(leftPadding, 3, "Difficulty: " + calculateDifficulty() + "x");
+            putString(leftPadding, 3, "Difficulty: " + calculateDifficulty() + "x");
         }
         else {
-            writeString(leftPadding, 3, "Gravity: " + gameState.getGravity().toMillis() + " ms");
+            putString(leftPadding, 3, "Gravity: " + gameState.getGravity().toMillis() + " ms");
         }
 
         // Hold Block
         String holdName = "Hold";
         int holdY = 5;
-        writeString(leftPadding, holdY, holdName);
+        putString(leftPadding, holdY, holdName);
         BlockData blockData = gameState.getHeldBlockCopy();
         if (blockData != null) {
             writeBlock(leftPadding + holdName.length() + 1, holdY, blockData.getCellCopy(), SOLID);
@@ -69,26 +57,10 @@ public class GameRenderer {
 
         // Queue
         int textQueueY = 8;
-        writeString(leftPadding, textQueueY, "Next");
+        putString(leftPadding, textQueueY, "Next");
         writeQueue(leftPadding + 2, textQueueY + 2);
 
-        updateBufferAndPrint();
-    }
-
-    private void updateBufferAndPrint() {
-        for (int row = 0; row < BUFFER_HEIGHT; row++ ) {
-            for (int col = 0; col < BUFFER_WIDTH; col++) {
-                if (!previousBuffer[row][col].isEquals(currentBuffer[row][col])) {
-                    RenderCell cell = currentBuffer[row][col];
-                    draw(col, row, cell);
-                }
-            }
-        }
         terminal.flush();
-
-        previousBuffer = currentBuffer;
-        currentBuffer = new RenderCell[BUFFER_HEIGHT][BUFFER_WIDTH];
-        clearBuffer(currentBuffer);
     }
 
     // =====================
@@ -98,9 +70,9 @@ public class GameRenderer {
         for (int row = 0; row < ySize; row++) {
             for (int col = 0; col < xSize; col++) {
                 if ((row == 0 || row == ySize - 1 ||
-                        col == 0 || col == xSize - 1) && isWithinBuffer(row, col)
+                        col == 0 || col == xSize - 1)
                 ) {
-                    currentBuffer[row + y][col + x] = new RenderCell(SOLID, Color.GREY);
+                    putCell(col + x, row + y, SOLID, Color.GREY);
                 }
             }
         }
@@ -130,8 +102,8 @@ public class GameRenderer {
                 int rowOffset = row + BORDER_THICKNESS;
                 int colOffset = col + BORDER_THICKNESS;
 
-                if (!cell.isEmpty() && isWithinBuffer(rowOffset, colOffset)) {
-                    currentBuffer[rowOffset][colOffset] = new RenderCell(SOLID, cell.color);
+                if (!cell.isEmpty()) {
+                    putCell(colOffset, rowOffset,SOLID, cell.color);
                 }
             }
         }
@@ -158,33 +130,15 @@ public class GameRenderer {
     // =====================
     // Helpers
     // =====================
-    private void writeBlock(int colStart, int rowStart, Cell[][] block, char out) {
+    private void writeBlock(int colStart, int rowStart, Cell[][] block, String out) {
         for (int row = 0; row < block.length; row++) {
             for (int col = 0; col < block[0].length; col++) {
                 Cell cell = block[row][col];
-                int rowOffset = row + rowStart;
-                int colOffset = col + colStart;
-
-                if (!cell.isEmpty() && isWithinBuffer(rowOffset, colOffset)) {
-                    currentBuffer[row + rowStart][col + colStart] = new RenderCell(out, cell.color);
+                if (!cell.isEmpty()) {
+                    putCell(col + colStart, row + rowStart, out, cell.color);
                 }
             }
         }
-    }
-
-    private void writeString(int col, int row, String out) {
-        char[] charArray = out.toCharArray();
-
-        for (int pointer = 0; pointer < charArray.length; pointer++) {
-            if (isWithinBuffer(row, col + pointer)) {
-                currentBuffer[row][col + pointer] = new RenderCell(charArray[pointer], Color.WHITE);
-                currentBuffer[row][col + pointer].isCharacter = true;
-            }
-        }
-    }
-
-    private boolean isWithinBuffer(int row, int col) {
-        return row < BUFFER_HEIGHT && col < BUFFER_WIDTH;
     }
 
     private double calculateDifficulty() {
@@ -192,37 +146,31 @@ public class GameRenderer {
         return Math.ceil((double) 500 / gameState.getGravity().toMillis() * 100) / 100;
     }
 
-    private void clearBuffer(RenderCell[][] buffer) {
-        for (int row = 0; row < BUFFER_HEIGHT; row++) {
-            for (int col = 0; col < BUFFER_WIDTH; col++) {
-                buffer[row][col] = new RenderCell(EMPTY, Color.DEFAULT);
-                buffer[row][col].isEmpty = true;
-            }
-        }
-    }
-
     // =====================
     // Screen Drawing
     // =====================
-    private void draw(int col, int row, RenderCell cell) {
+    private void putCell(int col, int row, String out, Color color) {
         int colOffset = col * 2;
 
-        if (colOffset > terminal.getXSize() || colOffset + 1 > terminal.getXSize() || row > terminal.getYSize()) {
-            return;
-        }
-
-        int[] textColor = getTextColor(cell.color);
+        int[] textColor = getTextColor(color);
         terminal.setForegroundColor(textColor[0], textColor[1], textColor[2]);
 
-        char out1 = cell.symbol;
-        char out2 = out1;
+        terminal.put(colOffset, row, out);
+        terminal.put(colOffset + 1, row, out);
 
-        if (cell.isCharacter) {
-            out2 = ' ';
+        terminal.resetColorAndStyle();
+    }
+
+    private void putString(int col, int row, String out) {
+        int colOffset = col * 2;
+
+        char[] charArray = out.toCharArray();
+
+        terminal.setForegroundColor(255, 255, 255);
+
+        for (int pointer = 0; pointer < charArray.length; pointer++) {
+            terminal.put(colOffset + pointer * 2, row, String.valueOf(charArray[pointer]));
         }
-
-        terminal.put(colOffset, row, String.valueOf(out1));
-        terminal.put(colOffset + 1, row, String.valueOf(out2));
 
         terminal.resetColorAndStyle();
     }
@@ -240,25 +188,5 @@ public class GameRenderer {
             case CYAN -> new int[]{0, 205, 205};
             case WHITE -> new int[]{255, 255, 255};
         };
-    }
-
-    private static class  RenderCell {
-        private final char symbol;
-        private final Color color;
-
-        private boolean isEmpty;
-        private boolean isCharacter;
-
-        public RenderCell(char symbol, Color color) {
-            this.symbol = symbol;
-            this.color = color;
-        }
-
-        public boolean isEquals(RenderCell renderCell) {
-            return this.symbol == renderCell.symbol &&
-                this.color == renderCell.color &&
-                this.isEmpty == renderCell.isEmpty &&
-                this.isCharacter == renderCell.isCharacter;
-        }
     }
 }
