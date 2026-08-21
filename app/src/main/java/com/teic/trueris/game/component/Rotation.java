9479c3f6294c3d8843a7d0d272790ef92729337c
BFUR64 <iterranceevan@gmail.com>
2026-08-21 09:20:51 +0800

    refactor: Add `world` ECS and basic components
    Added Components:
    - Position
    - Rotation
    - Shape

---
+++ app/src/main/java/com/teic/trueris/game/component/Rotation.java
@@ -0,0 +1,7 @@
+package com.teic.trueris.game.component;
+
+import com.teic.trueris.game.block.Direction;
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
+public record Rotation(Direction direction) {}
