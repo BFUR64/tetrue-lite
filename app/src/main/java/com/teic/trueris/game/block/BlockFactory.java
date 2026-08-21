becfce55f89ba509312a265fead7a218803eb471
BFUR64 <iterranceevan@gmail.com>
2026-08-21 11:52:51 +0800

    feat: Add basic `BlockFactory`
    This is for when to create new blocks. Its purpose is to be used by the GameManager and SevenBag randomizer in the future.

---
+++ app/src/main/java/com/teic/trueris/game/block/BlockFactory.java
@@ -0,0 +1,27 @@
+package com.teic.trueris.game.block;
+
+import com.teic.trueris.game.World;
+import com.teic.trueris.game.cell.Cell;
+import com.teic.trueris.game.component.Position;
+import com.teic.trueris.game.component.Rotation;
+import com.teic.trueris.game.component.Shape;
+
+public class BlockFactory {
+    private final World world;
+
+    private int nextBlockId = 0;
+
+    public BlockFactory(World world) {
+        this.world = world;
+    }
+
+    public Integer createBlock(Cell cell) {
+        int id = nextBlockId++;
+
+        world.add(id, new Position(0, 0));
+        world.add(id, new Rotation(Direction.UP));
+        world.add(id, new Shape());
+
+        return id;
+    }
+}
