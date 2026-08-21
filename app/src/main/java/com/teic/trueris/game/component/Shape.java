eeb02687f883b98e70d2872d8c7522c3f71d2da2
BFUR64 <iterranceevan@gmail.com>
2026-08-21 18:48:24 +0800

    refactor: Add `BlockTemplate` to `Shape` component

--- app/src/main/java/com/teic/trueris/game/component/Shape.java
+++ app/src/main/java/com/teic/trueris/game/component/Shape.java
@@ -1,6 +1,7 @@
 package com.teic.trueris.game.component;
 
+import com.teic.trueris.game.block.BlockTemplate;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
-public record Shape() {}
+public record Shape(BlockTemplate blockTemplate) {}
