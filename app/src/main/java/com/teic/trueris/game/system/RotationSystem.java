32fd0009729e35869d7884b6cb25fd4c19a5729c
BFUR64 <iterranceevan@gmail.com>
2026-08-22 12:22:59 +0800

    refactor: Extract `getCell` `setCell` into utility

--- app/src/main/java/com/teic/trueris/game/system/RotationSystem.java
+++ app/src/main/java/com/teic/trueris/game/system/RotationSystem.java
@@ -9,9 +9,12 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static com.teic.trueris.game.utils.CellGrid.getCell;
+import static com.teic.trueris.game.utils.CellGrid.setCell;
+
 @NullMarked
 public class RotationSystem {
-    public List<@Nullable CellType> rotateBlockNTimes(int amount, BlockTemplate blockTemplate) {
+    public static List<@Nullable CellType> rotateBlockNTimes(int amount, BlockTemplate blockTemplate) {
         int size = blockTemplate.size();
 
         @Nullable CellType[] cells = blockTemplate.cells().toArray(new CellType[0]);
@@ -24,7 +27,7 @@ public class RotationSystem {
         return Collections.unmodifiableList(Arrays.asList(cells));
     }
 
-    private @Nullable CellType[] rotateArrayRight(int size, @Nullable CellType[] cells) {
+    private static @Nullable CellType[] rotateArrayRight(int size, @Nullable CellType[] cells) {
         @Nullable CellType[] newCells = new CellType[size * size];
 
         for (int row = 0; row < size; row++) {
@@ -34,13 +37,5 @@ public class RotationSystem {
         }
 
         return newCells;
-    }
-
-    public static @Nullable CellType getCell(@Nullable CellType[] cells, int width, int x, int y) {
-        return cells[y * width + x];
-    }
-
-    public static void setCell(@Nullable CellType[] cells, int width, int x, int y, @Nullable CellType cellType) {
-        cells[y * width + x] = cellType;
     }
 }
---
+++ app/src/main/java/com/teic/trueris/game/utils/CellGrid.java
@@ -0,0 +1,20 @@
+package com.teic.trueris.game.utils;
+
+import com.teic.trueris.game.cell.CellType;
+import org.jspecify.annotations.Nullable;
+
+import java.util.List;
+
+public class CellGrid {
+    public static @Nullable CellType getCell(@Nullable CellType[] cells, int width, int x, int y) {
+        return cells[y * width + x];
+    }
+
+    public static @Nullable CellType getCell(List<@Nullable CellType> cells, int width, int x, int y) {
+        return cells.get(y * width + x);
+    }
+
+    public static void setCell(@Nullable CellType[] cells, int width, int x, int y, @Nullable CellType cellType) {
+        cells[y * width + x] = cellType;
+    }
+}
