0560ab955870cfe245b5dc8c9011e0d6b01668f9
BFUR64 <iterranceevan@gmail.com>
2026-08-24 16:13:22 +0800

    fix: Add log4j-core to fix warning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation("org.jspecify:jspecify:1.0.0")
     implementation("io.github.bfur64:menu-manager:0.9.2")
     implementation("io.github.bfur64:micro-sound:0.1.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.26.0")
     compileOnly("com.github.spotbugs:spotbugs-annotations:4.10.4")
 }
 
