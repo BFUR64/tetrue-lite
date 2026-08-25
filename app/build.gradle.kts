7a850a0425c4d27e1ef792c0317eec4003c68da2
BFUR64 <iterranceevan@gmail.com>
2026-08-25 17:40:39 +0800

    feat: Add packaged Tetrue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,3 +33,64 @@ java {
 application {
     mainClass = "com.teic.trueris.App"
 }
+
+val projectName = project.name
+
+val installDir = layout.buildDirectory.dir("install/$projectName")
+val inputDir = installDir.get().asFile.resolve("lib")
+
+val runtimeDir = layout.buildDirectory.dir("runtime").get().asFile
+val packageDir = layout.buildDirectory.dir("jpackage").get().asFile
+
+val mainClass = application.mainClass.get()
+
+val iconPath = layout.projectDirectory
+    .file("src/main/packaging/logo.ico")
+    .asFile
+    .absolutePath
+
+tasks.register<Exec>("createRuntime") {
+    dependsOn(tasks.installDist)
+
+    // To get the modules, run:
+    // jdeps `
+    //     --print-module-deps `
+    //     --ignore-missing-deps `
+    //     --multi-release 21 `
+    //     --class-path "..\install\app\lib\*" `
+    //     (Get-ChildItem "..\install\app\lib\*.jar").FullName
+    // On app/build/libs
+
+    delete(runtimeDir)
+
+    commandLine(
+        "jlink",
+        "--module-path", "${System.getProperty("java.home")}/jmods",
+        "--add-modules", "java.base,java.desktop,java.management,jdk.httpserver",
+        "--strip-debug",
+        "--no-man-pages",
+        "--no-header-files",
+        "--output", runtimeDir.absolutePath
+    )
+}
+
+tasks.register<Exec>("packageWindows") {
+    description = "Make a jpackage application image for Windows"
+
+    dependsOn("build")
+
+    delete(packageDir)
+
+    commandLine(
+        "jpackage",
+        "--type", "app-image",
+        "--name", "Tetrue",
+        "--input", inputDir.absolutePath,
+        "--main-jar", "$projectName.jar",
+        "--main-class", mainClass,
+        "--dest", packageDir.absolutePath,
+        "--runtime-image", runtimeDir.absolutePath,
+        "--win-console",
+        "--icon", iconPath
+    )
+}
