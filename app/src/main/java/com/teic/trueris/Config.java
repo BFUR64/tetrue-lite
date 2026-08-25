9af01a1b51d96f364ced499d8688a3d03e45dbac
BFUR64 <iterranceevan@gmail.com>
2026-08-25 08:57:13 +0800

    release: Bump to 3.1.0

--- app/src/main/java/com/teic/trueris/Config.java
+++ app/src/main/java/com/teic/trueris/Config.java
@@ -7,7 +7,7 @@ import org.jspecify.annotations.NullMarked;
 
 @NullMarked
 public class Config {
-    public static final String GAME_VERSION = "v3.0.1";
+    public static final String GAME_VERSION = "v3.1.0";
 
     // milliseconds
     private static int gravityDef = 500;
